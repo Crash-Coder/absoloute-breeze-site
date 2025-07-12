a14f8763ea76bebe3226ac644e614aab2890d834
Crash-Coder <sawyerzachary00@gmail.com>
2025-07-11 20:25:53 -0400

    basic html on vendors, our-story, get-in-touch, and header work.

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
 
-hamburger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
-});
 
-mobileMenu.addEventListener('click', () => {
-    mobileMenu.classList.remove('active');
-});
+async function loadHeader() {
+    const res = await fetch('header.html');
+    const html = await res.text();
+    document.getElementById('header').innerHTML = html;
 
+
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+
+    hamburger.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+    });
+
+    mobileMenu.addEventListener('click', () => {
+        mobileMenu.classList.remove('active');
+    });
+
+}
+
+
+loadHeader();
