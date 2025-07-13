7f64709a1959e2f39d17640449dad134c08ec546
Crash-Coder <sawyerzachary00@gmail.com>
2025-07-13 14:59:12 -0400

    Finished version of site

--- app.js
+++ app.js
@@ -20,5 +20,12 @@ async function loadHeader() {
 
 }
 
+async function loadFooter() {
+    const res = await fetch('footer.html');
+    const html = await res.text();
+    document.getElementById('footer').innerHTML = html;
+}
+
 
 loadHeader();
+loadFooter();
